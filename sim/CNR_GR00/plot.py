976ed345edf5f5fa9643aa67dc89d36898a73d20
Carsten Wulff <carsten@wulff.no>
2023-12-10 21:52:40 +0100

    Extended to 20 us

--- sim/CNR_GR00/plot.py
+++ sim/CNR_GR00/plot.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 import yaml
 import sys
+import pandas as pd
 import matplotlib.pyplot as plt
 
-files = sys.argv[1:]
+frun = sys.argv[1]
 
-temp = list()
-vals = list()
+files = list()
+with open(frun) as fi:
+    for line in fi:
+        files.append(line.strip() + ".yaml")
+
+
 
 for fname in files:
     with open(fname) as fi:
         obj = yaml.safe_load(fi)
+
+    data = dict()
+    vals = list()
     for k in obj:
         if("do" in k):
             (dd,v) = k.split("do_")
-            temp.append(int(v))
-            vals.append(obj[k])
-        plt.plot(temp,vals,".")
+            vals.append([int(v),obj[k]])
+    df = pd.DataFrame(vals, columns=['x','y'])
+    df = df.sort_values(by=['x', 'y'])
+    plt.plot(df.x,df.y,label=fname)
 
+
+plt.xlabel("Temperature [C]")
+plt.ylabel("Output")
+plt.legend()
 plt.show()
